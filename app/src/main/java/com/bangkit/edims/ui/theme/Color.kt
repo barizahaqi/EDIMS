10784e8b740b5045d1965844e77b99032108a50a
barizahaqi <haqibariza@gmail.com>
2023-12-02 15:28:05 +0700

    main : set up theme

--- app/src/main/java/com/bangkit/edims/ui/theme/Color.kt
+++ app/src/main/java/com/bangkit/edims/ui/theme/Color.kt
@@ -1,11 +1,16 @@
+@file:Suppress("unused")
+
 package com.bangkit.edims.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Tacao = Color(0xFFF9B572)
+val PaleLeaf = Color(0xFFB7C7A6)
+val SurfCrest = Color(0xFFC3D0B4)
+val Beige = Color(0xFFFAF8ED)
+val Flamingo = Color(0xFFE55959)
+val Texas = Color(0xFFEEEA7E)
+val TurqoiseBlue = Color(0xFF6CD1F1)
+val Sulu = Color(0xFFB6E783)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
