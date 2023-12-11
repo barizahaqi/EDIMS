a17913b7a69845a3e0945082c1305821210ab9fe
Rizki <muhrizkiakb@gmail.com>
2023-12-11 17:54:08 +0800

    data : implementation database

---
+++ app/src/main/java/com/bangkit/edims/data/Result.kt
@@ -0,0 +1,7 @@
+package com.example.myproject.data
+
+sealed class Result<out T : Any?> {
+    object Loading : Result<Nothing>()
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val errorMessage: String) : Result<Nothing>()
+}
