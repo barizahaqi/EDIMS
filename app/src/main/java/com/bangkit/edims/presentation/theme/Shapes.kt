dbfb62ecf5f54be4d578aff018ba632c8f2c2ee4
Rizki <muhrizkiakb@gmail.com>
2023-12-19 17:36:54 +0800

    Update Shapes.kt

--- app/src/main/java/com/bangkit/edims/presentation/theme/Shapes.kt
+++ app/src/main/java/com/bangkit/edims/presentation/theme/Shapes.kt
@@ -1,4 +1,4 @@
-package com.example.myproject.ui.theme
+package com.bangkit.edims.presentation.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
