af14ca7fbfe571287213704a00992ec64702c74e
Rizki <muhrizkiakb@gmail.com>
2023-12-19 22:10:58 +0800

    Create ProductRepository.kt

---
+++ app/src/main/java/com/bangkit/edims/database/ProductRepository.kt
@@ -0,0 +1,25 @@
+package com.bangkit.edims.database
+
+import com.bangkit.edims.core.utils.ProductFilterType
+import kotlinx.coroutines.flow.Flow
+
+interface ProductRepository{
+
+    suspend fun insert(product: Product)
+
+    suspend fun delete(product: Product)
+
+    fun getItems(filter: ProductFilterType) : Flow<List<Product>>
+
+    fun getItemsById(id: Int) : Flow<Product>
+
+    fun getNearestItems() : List<Product>
+
+    suspend fun saveNotificationSettings(status: Boolean)
+
+    fun getNotificationSettings() : Flow<Boolean>
+
+    suspend fun saveAutoDeleteSettings(status: Boolean)
+
+    fun getAutoDeleteSettings() : Flow<Boolean>
+}
