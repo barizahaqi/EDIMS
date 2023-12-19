fee962f237ef450371f3cb068aec109a22927c0d
Rizki <muhrizkiakb@gmail.com>
2023-12-19 22:26:04 +0800

    Add files via upload

---
+++ app/src/main/java/com/bangkit/edims/presentation/components/camera/Camera.kt
@@ -0,0 +1,195 @@
+package com.bangkit.edims.presentation.component.camera
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.core.UseCase
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.bangkit.edims.core.utils.Permission
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
+suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { future ->
+        future.addListener({
+            continuation.resume(future.get())
+        }, executor)
+    }
+}
+
+val Context.executor: Executor
+    get() = ContextCompat.getMainExecutor(this)
+
+@Composable
+fun CameraPreview(
+    modifier: Modifier = Modifier,
+    scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+    onUseCase: (UseCase) -> Unit = {}
+) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+
+            onUseCase(Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+            )
+
+            previewView
+        }
+    )
+}
+
+@Composable
+fun CameraCapture(
+    modifier: Modifier = Modifier,
+    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
+    onImageFile: (File) -> Unit = {},
+    onDismiss: () -> Unit
+) {
+    val context = LocalContext.current
+    Permission(
+        permission = Manifest.permission.CAMERA,
+        rationale = "Please grant the permission.",
+        permissionNotAvailableContent = {
+            Column(modifier) {
+                Text("No Camera")
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(onClick = {
+                    context.startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = Uri.fromParts("package", context.packageName, null)
+                    })
+                }) {
+                    Text("Open Settings")
+                }
+            }
+        }
+    )
+
+    Box(modifier = modifier) {
+        val lifecycleOwner = LocalLifecycleOwner.current
+        var previewUseCase by remember {
+            mutableStateOf<UseCase>(Preview.Builder().build())
+        }
+        val imageCaptureUseCase by remember {
+            mutableStateOf(
+                ImageCapture.Builder()
+                    .setCaptureMode(CAPTURE_MODE_MAXIMIZE_QUALITY)
+                    .build()
+            )
+        }
+        val coroutineScope = rememberCoroutineScope()
+
+        CameraPreview(
+            modifier = Modifier.fillMaxSize(),
+            onUseCase = {
+                previewUseCase = it
+            }
+        )
+        Button(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(16.dp)
+                .align(Alignment.BottomCenter),
+            onClick = {
+                coroutineScope.launch {
+                    imageCaptureUseCase.takePicture(context.executor).let {
+                        onImageFile(it)
+                    }
+                    onDismiss()
+                }
+            }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Camera,
+                contentDescription = "Take Photo",
+            )
+        }
+        LaunchedEffect(previewUseCase) {
+            val cameraProvider = context.getCameraProvider()
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    lifecycleOwner, cameraSelector, previewUseCase, imageCaptureUseCase
+                )
+            } catch (e: Exception) {
+                Log.e("Camera", "Failed ", e)
+            }
+        }
+    }
+}
+
+suspend fun ImageCapture.takePicture(executor: Executor): File {
+    val photoFile = withContext(Dispatchers.IO) {
+        kotlin.runCatching {
+            File.createTempFile("image", "jpg")
+        }.getOrElse { e ->
+            Log.e("Take Picture", "Failed to create file ", e)
+            File("/dev/null")
+        }
+    }
+    return suspendCoroutine { continuation ->
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        takePicture(outputOptions, executor, object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                continuation.resume(photoFile)
+            }
+
+            override fun onError(e: ImageCaptureException) {
+                Log.e("Take Picture", "Failed to capture failed ", e)
+                continuation.resumeWithException(e)
+            }
+        })
+    }
+}
