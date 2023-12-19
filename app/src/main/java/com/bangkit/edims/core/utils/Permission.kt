91cc9a6ead5eec42cfef319b1ef756d1d2e476db
Rizki <muhrizkiakb@gmail.com>
2023-12-19 22:15:27 +0800

    Create Permission.kt

---
+++ app/src/main/java/com/bangkit/edims/core/utils/Permission.kt
@@ -0,0 +1,52 @@
+package com.bangkit.edims.core.utils
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionRequired
+import com.google.accompanist.permissions.rememberPermissionState
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun Permission(
+    permission: String = android.Manifest.permission.CAMERA,
+    rationale: String = "This permission is important for this app. Please grant the permission.",
+    permissionNotAvailableContent: @Composable () -> Unit = { },
+    content: @Composable () -> Unit = { }
+) {
+    val permissionState = rememberPermissionState(permission)
+    PermissionRequired(
+        permissionState = permissionState,
+        permissionNotGrantedContent = {
+            Rationale(
+                text = rationale,
+                onRequestPermission = { permissionState.launchPermissionRequest() }
+            )
+        },
+        permissionNotAvailableContent = permissionNotAvailableContent,
+        content = content
+    )
+}
+
+@Composable
+private fun Rationale(
+    text: String,
+    onRequestPermission: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = {},
+        title = {
+            Text(text = "Permission request")
+        },
+        text = {
+            Text(text)
+        },
+        confirmButton = {
+            Button(onClick = onRequestPermission) {
+                Text("Ok")
+            }
+        }
+    )
+}
