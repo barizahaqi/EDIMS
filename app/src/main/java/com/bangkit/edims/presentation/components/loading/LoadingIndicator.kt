191bd7969005149a0476b3ca015b1e7bb68678dc
Rizki <muhrizkiakb@gmail.com>
2023-12-20 21:55:00 +0800

    Add Loading Indicator

---
+++ app/src/main/java/com/bangkit/edims/presentation/components/loading/LoadingIndicator.kt
@@ -0,0 +1,117 @@
+package com.bangkit.edims.presentation.components.loading
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.bangkit.edims.presentation.theme.MyProjectTheme
+import com.bangkit.edims.presentation.theme.Orange2
+import kotlinx.coroutines.delay
+
+@Composable
+fun ShowLoading(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LoadingIndicator()
+    }
+}
+
+@Composable
+private fun LoadingIndicator(
+    modifier: Modifier = Modifier,
+    circleSize: Dp = 15.dp,
+    circleColor: Color = Orange2,
+    spaceBetween: Dp = 10.dp,
+    travelDistance: Dp = 15.dp,
+) {
+    val circle = listOf(
+        remember {
+            Animatable(initialValue = 0f)
+        },
+        remember {
+            Animatable(initialValue = 0f)
+        },
+        remember {
+            Animatable(initialValue = 0f)
+        },
+        remember {
+            Animatable(initialValue = 0f)
+        }
+    )
+    circle.forEachIndexed { index, animatable ->
+        LaunchedEffect(key1 = animatable) {
+            delay(index * 100L)
+            animatable.animateTo(
+                targetValue = 1f,
+                animationSpec = infiniteRepeatable(
+                    animation = keyframes {
+                        durationMillis = 1200
+                        0.0f at 0 using LinearOutSlowInEasing
+                        1.0f at 300 using LinearOutSlowInEasing
+                        0.0f at 600 using LinearOutSlowInEasing
+                        0.0f at 1200 using LinearOutSlowInEasing
+                    },
+                    repeatMode = RepeatMode.Restart
+                )
+            )
+        }
+    }
+
+    val circleValues = circle.map { it.value }
+    val distance = with(LocalDensity.current) { travelDistance.toPx() }
+    val lastCircle = circleValues.size - 1
+
+    Row(
+        modifier = modifier
+    ) {
+        circleValues.forEachIndexed { index, value ->
+            Box(
+                modifier = Modifier
+                    .size(circleSize)
+                    .graphicsLayer {
+                        translationY = -value * distance
+                    }
+                    .background(
+                        color = circleColor,
+                        shape = CircleShape
+                    ),
+            )
+
+            if (index != lastCircle) {
+                Spacer(modifier = Modifier.width(spaceBetween))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoadingPreview() {
+    MyProjectTheme {
+        ShowLoading()
+    }
+}
