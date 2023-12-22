d3d6e16981598b5eacbc234d465b0124273f243b
Rizki <muhrizkiakb@gmail.com>
2023-12-22 17:17:07 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,4 +106,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    implementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
 }
