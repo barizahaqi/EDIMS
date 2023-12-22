8cfc9c92afb33ebc7d35210786a89b469519b0c7
Rizki <muhrizkiakb@gmail.com>
2023-12-22 17:09:49 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,4 +101,9 @@ dependencies {
 
     //icons extended
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
