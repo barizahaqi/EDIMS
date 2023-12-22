81648ea964e94ae0339803bc191f00fc234e4cfa
Rizki <muhrizkiakb@gmail.com>
2023-12-22 17:15:30 +0800

    Update ProductRepository.kt

--- app/src/main/java/com/bangkit/edims/database/ProductRepository.kt
+++ app/src/main/java/com/bangkit/edims/database/ProductRepository.kt
@@ -1,11 +1,36 @@
 package com.bangkit.edims.database
 
 import com.bangkit.edims.core.utils.ProductFilterType
+import com.bangkit.edims.data.Result
+import com.bangkit.edims.data.User
+import com.bangkit.edims.data.retrofit.ErrorResponse
+import com.bangkit.edims.data.retrofit.ItemsResponse
+import com.bangkit.edims.data.retrofit.LoginResponse
+import com.bangkit.edims.data.retrofit.SignupResponse
+import com.bangkit.edims.data.retrofit.UploadResponse
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 interface ProductRepository{
 
+    fun isDataFetched() : Boolean
+
+    fun login(email: String, password: String) : Flow<Result<LoginResponse>>
+
+    fun signup(email: String, username : String, password: String) : Flow<Result<SignupResponse>>
+
+    fun getAllData() : Flow<Result<ItemsResponse>>
+
+    fun uploadProductApi(name: RequestBody, imageFile: MultipartBody.Part, category: RequestBody, date: RequestBody) : Flow<Result<UploadResponse>>
+
+    fun deleteProductApi(id : Int) : Flow<Result<ErrorResponse>>
+
+    suspend fun logout()
+
     suspend fun insert(product: Product)
+
+    suspend fun insertAll(listProduct: List<Product>)
 
     suspend fun delete(product: Product)
 
@@ -22,4 +47,14 @@ interface ProductRepository{
     suspend fun saveAutoDeleteSettings(status: Boolean)
 
     fun getAutoDeleteSettings() : Flow<Boolean>
+
+    suspend fun saveLoginData(user : User)
+
+    suspend fun clearLoginData()
+
+    fun getUserData() : Flow<User>
+
+    fun getToken() : Flow<String>
+
+    suspend fun changeImageProfile (imageProfile: String)
 }
